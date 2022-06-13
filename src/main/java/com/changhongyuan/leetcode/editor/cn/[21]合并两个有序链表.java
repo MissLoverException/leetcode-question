4f0ea3f7da1caf47f5b96b62a295fd0a417bd2a4
changhongyuan <changhongyuan@sumec.com.cn>
2022-06-13 10:38:29 +0800

    21题

--- src/main/java/com/changhongyuan/leetcode/editor/cn/[21]合并两个有序链表.java
+++ src/main/java/com/changhongyuan/leetcode/editor/cn/[21]合并两个有序链表.java
@@ -52,12 +52,28 @@ class MergeTwoSortedLists{
  */
 class Solution {
     public ListNode mergeTwoLists(ListNode list1, ListNode list2) {
-        return null;
+        if (null == list1) {
+            return list2;
+        }
+        if (null == list2) {
+            return list1;
+        }
+        if (list1.val < list2.val) {
+            list1.next = mergeTwoLists(list1.next, list2);
+            return list1;
+        } else {
+            list2.next = mergeTwoLists(list2.next, list1);
+            return list2;
+        }
     }
 }
 //leetcode submit region end(Prohibit modification and deletion)
 
     public static void main(String[] args) {
         Solution solution = new MergeTwoSortedLists().new Solution();
+        ListNode list1 = new ListNode(1, new ListNode(2, new ListNode(4)));
+        ListNode list2 = new ListNode(1, new ListNode(3, new ListNode(4)));
+        ListNode result = solution.mergeTwoLists(list1, list2);
+        System.out.println(result.toString());
     }
 }
